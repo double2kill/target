3b397404cb1d75f0452320948a4c3037a3e41512
double2kill <379563000@qq.com>
2022-02-15 01:13:33 +0800

    feat: 🎸 增加默认排序方式

--- frontend/src/pages/Mobile/List.vue
+++ frontend/src/pages/Mobile/List.vue
@@ -5,7 +5,7 @@ import { onMounted } from 'vue'
 
 import FormattedDate from '../../components/Date/FormattedDate.vue'
 
-import { listData, fetchData, handleCompleteItem, 目标是今日目标, 目标已超时 } from './logic'
+import { listData, fetchData, handleCompleteItem, handleCancelItem, 目标是今日目标, 目标已超时, 目标是本周目标 } from './logic'
 
 onMounted(() => {
   fetchData()
@@ -13,41 +13,58 @@ onMounted(() => {
 
 </script>
 <template>
-  <CellGroup>
-    <Cell
-      v-for="item in listData"
-      :key="item.id"
-      :title="item.目标内容"
-    >
-      <template #label>
-        <FormattedDate
-          :value="item.计划完成时间"
-        />
-        <Tag
-          v-if="目标是今日目标(item)"
-          type="warning"
-          class="tag-item"
-        >
-          今日目标
-        </Tag>
-        <Tag
-          v-if="目标已超时(item)"
-          type="danger"
-          class="tag-item"
-        >
-          已超时
-        </Tag>
-        <Button
-          type="primary"
-          size="mini"
-          class="action"
-          @click="handleCompleteItem(item)"
-        >
-          完成
-        </Button>
-      </template>
-    </Cell>
-  </CellGroup>
+  <div>
+    <CellGroup>
+      <Cell
+        v-for="item in listData"
+        :key="item.id"
+        :title="item.目标内容"
+      >
+        <template #label>
+          <FormattedDate
+            :value="item.计划完成时间"
+          />
+          <Tag
+            v-if="目标是今日目标(item)"
+            type="primary"
+            class="tag-item"
+          >
+            今日目标
+          </Tag>
+          <Tag
+            v-if="目标是本周目标(item)"
+            type="warning"
+            class="tag-item"
+          >
+            本周目标
+          </Tag>
+          <Tag
+            v-if="目标已超时(item)"
+            type="danger"
+            class="tag-item"
+          >
+            已超时
+          </Tag>
+          <span class="action">
+            <Button
+              v-if="目标已超时(item)"
+              size="mini"
+              @click="handleCancelItem(item)"
+            >
+              取消
+            </Button>
+            <Button
+              type="primary"
+              size="mini"
+              @click="handleCompleteItem(item)"
+            >
+              完成
+            </Button>
+          </span>
+        </template>
+      </Cell>
+    </CellGroup>
+  </div>
 </template>
 
 <style>
--- frontend/src/pages/Mobile/logic.ts
+++ frontend/src/pages/Mobile/logic.ts
@@ -28,11 +28,31 @@ export const fetchData = async () => {
   listData.value = result.data
     .filter(item => !item.完成时间)
     .sort((a,b) => {
+      if (目标是今日目标(a) && !目标是今日目标(b)) {
+        return -1
+      }
+      if (!目标是今日目标(a) && 目标是今日目标(b)) {
+        return 1
+      }
+      if (目标是本周目标(a) && !目标是本周目标(b)) {
+        return -1
+      }
+      if (!目标是本周目标(a) && 目标是本周目标(b)) {
+        return 1
+      }
       if (!目标已超时(a) && !目标已超时(b)) {
         return a.计划完成时间 - b.计划完成时间
       }
       return b.计划完成时间 - a.计划完成时间
     })
+}
+
+export const handleCancelItem = async (rowData: TargetItem) => {
+  await axios.patch(`${TARGET_LIST}/${rowData.id}`, {
+    完成时间: new Date().valueOf(),
+    备注: '已取消'
+  })
+  await fetchData()
 }
 
 export const handleCompleteItem = async (rowData: TargetItem) => {
@@ -44,3 +64,4 @@ export const handleCompleteItem = async (rowData: TargetItem) => {
 
 export const 目标是今日目标 = (item: TargetItem) => dayjs(item.计划完成时间).isSame(dayjs(), 'day')
 export const 目标已超时 = (item: TargetItem) => item.计划完成时间 < new Date().valueOf()
+export const 目标是本周目标 = (item: TargetItem) => dayjs(item.计划完成时间).isSame(dayjs(), 'week')
