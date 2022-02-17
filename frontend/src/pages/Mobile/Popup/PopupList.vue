8d646bca3db7a38c58100179438ddfe111adeae1
double2kill <379563000@qq.com>
2022-02-18 01:54:23 +0800

    feat: 🎸 添加路由守卫解决返回键问题

--- frontend/src/pages/Mobile/Popup/PopupList.vue
+++ frontend/src/pages/Mobile/Popup/PopupList.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import {onBeforeRouteLeave} from 'vue-router'
+
 import DetailPopup from './DetailPopup.vue'
+import {是否显示详情弹出层, 操作关闭详情弹出层} from './logic'
+
+onBeforeRouteLeave((to) => {
+  if (是否显示详情弹出层.value) {
+    操作关闭详情弹出层()
+    return false
+  }
+  return true
+})
 </script>
 <template>
   <DetailPopup />
