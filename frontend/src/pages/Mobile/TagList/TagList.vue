a38aec24852cb3642e73cf2923a7d0fda51eb4c3
double2kill <379563000@qq.com>
2022-02-27 00:14:25 +0800

    feat: 🎸 增加剩余时间

--- frontend/src/pages/Mobile/TagList/TagList.vue
+++ frontend/src/pages/Mobile/TagList/TagList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
+import dayjs from 'dayjs'
 import { Tag } from 'vant'
-import { PropType } from 'vue'
+import { PropType, computed } from 'vue'
 
 import {TargetItem} from '../../../components/type'
 import { 目标是今日目标, 目标已超时, 目标是本周目标 } from '../logic'
@@ -10,6 +11,25 @@ const props = defineProps({
     type: Object as PropType<TargetItem>,
     required: true,
   },
+})
+
+const 至今差别天数 = computed(() => {
+  const 计划完成时间Dayjs= dayjs(props.value.计划完成时间)
+  const 当前Dayjs = dayjs()
+  const 天数 = 计划完成时间Dayjs.diff(当前Dayjs, 'day')
+  if (天数 < 7) {
+    return `${天数}天后`
+  }
+  const 周数 = 计划完成时间Dayjs.diff(当前Dayjs, 'week')
+  if (周数 < 5) {
+    return `${周数}周后`
+  }
+  const 月数 = 计划完成时间Dayjs.diff(当前Dayjs, 'month')
+  if (月数 < 12) {
+    return `${月数}个月后`
+  }
+  const 年数 = 计划完成时间Dayjs.diff(当前Dayjs, 'month')
+  return `${年数}月后`
 })
 
 </script>
@@ -36,6 +56,14 @@ const props = defineProps({
       class="tag-item"
     >
       已超时
+    </Tag>
+    <Tag
+      v-if="!(目标是本周目标(props.value)||目标已超时(props.value))"
+      plain
+      type="warning"
+      class="tag-item"
+    >
+      {{ 至今差别天数 }}
     </Tag>
   </span>
 </template>
